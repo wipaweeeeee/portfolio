da854233fef346572070ef3bab5d7202cdd3de31
Wipawe Sirikolkarn <wsirikolkarn@gmail.com>
2019-10-09 18:39:22 -0400

    omg mouse has image now

--- src/packages/landing/Landing.js
+++ src/packages/landing/Landing.js
@@ -6,6 +6,7 @@ import cc from '../../assets/images/cc.gif';
 import vb from '../../assets/images/vb.jpg';
 import f50 from '../../assets/images/f50.png';
 import ProjectCarousel from '../carousel/ProjectCarousel';
+import arrow from '../../assets/images/arrow.png';
 
 let array = {images: [f50, vb, cc], name: "2019 | frog 50", legend: "AR (Unity, Vuforia), C4D, iOS"};
 
@@ -14,6 +15,8 @@ const Landing = (props) => {
 	const [ content, setContent] = useState();
 	const [ name, setName ] = useState();
 	const [ legend, setLegend ] = useState();
+	const [ mouse, setMouse ] = useState({x: 0, y: 0});
+	const [ showMouse, setShowMouse ] = useState(false);
 
 	const handleShowProject = (e, project) => {
 		setProject(project);
@@ -28,8 +31,19 @@ const Landing = (props) => {
 		setLegend(content.legend);
 	}
 
+	const handleMouse = (e) => {
+		setMouse({x: e.clientX - 95, y: e.clientY});
+
+		if ( e.clientX > window.innerWidth/2) {
+			setShowMouse(true);
+		} else {
+			setShowMouse(false);
+		}
+	}
+
 	return (
-		<div className={styles.landing}>
+		<div className={styles.landing} onMouseMove={(e) => {handleMouse(e)}}>
+		<img src={arrow} style={{position: 'absolute', top: mouse.y, left: mouse.x}} className={classNames(styles.mouseImage, {[styles.showMouse] : showMouse})} width="190" height="23" alt="mouse"/>
 		<div className={styles.info}>
 			<div className={styles.intro}>
 				Wipawe here. 
