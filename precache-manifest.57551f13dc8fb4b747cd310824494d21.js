89e6a75a2f8af815570d268ae4d9a9b9306e7aa7
Wipawe Sirikolkarn <wsirikolkarn@gmail.com>
2019-10-17 12:35:34 -0400

    Updates

---
+++ precache-manifest.57551f13dc8fb4b747cd310824494d21.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e46ea5c36d6f98e3d9407824170bc621",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "29539d9d2ba92df40314",
+    "url": "/portfolio/static/css/main.771c262b.chunk.css"
+  },
+  {
+    "revision": "90eb979fc9fe4b59f0ef",
+    "url": "/portfolio/static/js/2.403b1c46.chunk.js"
+  },
+  {
+    "revision": "29539d9d2ba92df40314",
+    "url": "/portfolio/static/js/main.8c3e5863.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "53d8db45595548d7e49a4cad4df27bcf",
+    "url": "/portfolio/static/media/TitlingGothicFBExtendedBlac.53d8db45.woff"
+  },
+  {
+    "revision": "cd396290a0e6ed329b4cba02c1db2561",
+    "url": "/portfolio/static/media/apple.cd396290.gif"
+  },
+  {
+    "revision": "aa8157c9f6bbd96dabe91cd67847c795",
+    "url": "/portfolio/static/media/ar_2.aa8157c9.gif"
+  },
+  {
+    "revision": "04806ce91d6ca9ef44976b3156708995",
+    "url": "/portfolio/static/media/cc.04806ce9.gif"
+  },
+  {
+    "revision": "8e729412a7316e20bf5471bcde8cd90d",
+    "url": "/portfolio/static/media/f50.8e729412.png"
+  },
+  {
+    "revision": "58f013d9075a96a27d127b31ea6d221c",
+    "url": "/portfolio/static/media/lunchtype22-regular-webfont.58f013d9.woff"
+  },
+  {
+    "revision": "01d15242deae35eb5d993f88e4148a55",
+    "url": "/portfolio/static/media/tattoo.01d15242.gif"
+  },
+  {
+    "revision": "c87d9ac223d7b87ff856fecbfb273ffe",
+    "url": "/portfolio/static/media/wall.c87d9ac2.JPG"
+  },
+  {
+    "revision": "7fff4d6d1c7cdca147cf61b6c5b4e6b5",
+    "url": "/portfolio/static/media/yamaha.7fff4d6d.gif"
+  }
+]);
