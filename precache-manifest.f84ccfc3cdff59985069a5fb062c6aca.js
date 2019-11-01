c88b29e8213233b0e8518c6326307b6626512a1a
Wipawe Sirikolkarn <wsirikolkarn@gmail.com>
2019-11-01 15:18:39 -0400

    Updates

---
+++ precache-manifest.f84ccfc3cdff59985069a5fb062c6aca.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c6a412f5f8c56018d6926c935461c99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8ab66a8deb8fb60fe0f",
+    "url": "/static/css/main.aa6b4668.chunk.css"
+  },
+  {
+    "revision": "5901044ecbdcfb28132c",
+    "url": "/static/js/2.d2f96de1.chunk.js"
+  },
+  {
+    "revision": "b8ab66a8deb8fb60fe0f",
+    "url": "/static/js/main.e154e7d6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "53d8db45595548d7e49a4cad4df27bcf",
+    "url": "/static/media/TitlingGothicFBExtendedBlac.53d8db45.woff"
+  },
+  {
+    "revision": "51e673dd7bb920d751e0dcbcd7636400",
+    "url": "/static/media/adele.51e673dd.png"
+  },
+  {
+    "revision": "8b63165afb0a20787150e164a9ddb7fb",
+    "url": "/static/media/adele_2.8b63165a.png"
+  },
+  {
+    "revision": "2ab9d25c74a1d0cb44e066d05cce92fd",
+    "url": "/static/media/adele_3.2ab9d25c.png"
+  },
+  {
+    "revision": "f9ec860591d08e14e741e994293ba478",
+    "url": "/static/media/adele_4.f9ec8605.png"
+  },
+  {
+    "revision": "b7084627d16745bc2dec8bb116e3db4d",
+    "url": "/static/media/adele_5.b7084627.png"
+  },
+  {
+    "revision": "094d7aa57c412e250ee0fb543acfd7cb",
+    "url": "/static/media/adele_6.094d7aa5.png"
+  },
+  {
+    "revision": "32477e98f23527d29a0ff520b7ae0f79",
+    "url": "/static/media/adele_7.32477e98.png"
+  },
+  {
+    "revision": "8d50efe9b40bcc3eae8cb7b3939010e3",
+    "url": "/static/media/adele_8.8d50efe9.jpg"
+  },
+  {
+    "revision": "aa8157c9f6bbd96dabe91cd67847c795",
+    "url": "/static/media/ar_2.aa8157c9.gif"
+  },
+  {
+    "revision": "04806ce91d6ca9ef44976b3156708995",
+    "url": "/static/media/cc.04806ce9.gif"
+  },
+  {
+    "revision": "b7a4d1d681535984f1204266ced6f55d",
+    "url": "/static/media/eye.b7a4d1d6.png"
+  },
+  {
+    "revision": "ac2b1dee6a2dd7f09cb0fdc0d0958abe",
+    "url": "/static/media/eye_4.ac2b1dee.png"
+  },
+  {
+    "revision": "d0546f7caac02181a73156a34522d204",
+    "url": "/static/media/eye_5.d0546f7c.png"
+  },
+  {
+    "revision": "1c3abdad388726f5262c5c7dcfe70aa7",
+    "url": "/static/media/eye_6.1c3abdad.png"
+  },
+  {
+    "revision": "8e729412a7316e20bf5471bcde8cd90d",
+    "url": "/static/media/f50.8e729412.png"
+  },
+  {
+    "revision": "7f5abf456ed804e8157221337485a47d",
+    "url": "/static/media/flamingo.7f5abf45.png"
+  },
+  {
+    "revision": "72126ed928db621c7bf1cb28bbca7b39",
+    "url": "/static/media/freestyle.72126ed9.gif"
+  },
+  {
+    "revision": "64ea06a58f20aa2e0733e71a90aebf51",
+    "url": "/static/media/freestyleFP.64ea06a5.png"
+  },
+  {
+    "revision": "58f013d9075a96a27d127b31ea6d221c",
+    "url": "/static/media/lunchtype22-regular-webfont.58f013d9.woff"
+  },
+  {
+    "revision": "d520480fed68ca8f64c4b21da9051872",
+    "url": "/static/media/pump.d520480f.gif"
+  },
+  {
+    "revision": "c4d8b6fd575e37f9ebf0ae955e06fc3b",
+    "url": "/static/media/pumpFP.c4d8b6fd.png"
+  },
+  {
+    "revision": "464f1859df7158b4c2c1b8bf7ac42e7f",
+    "url": "/static/media/reebok-intro.464f1859.png"
+  },
+  {
+    "revision": "76bb205e24e33cd24aa940a4b555def5",
+    "url": "/static/media/tattoo-in-situ.76bb205e.gif"
+  },
+  {
+    "revision": "cf6a576a274c32c8f67aa6d67f888ae7",
+    "url": "/static/media/vb-intro.cf6a576a.png"
+  },
+  {
+    "revision": "c87d9ac223d7b87ff856fecbfb273ffe",
+    "url": "/static/media/wall.c87d9ac2.JPG"
+  },
+  {
+    "revision": "d0136fbc4d47f6004b1c496d13f0ea05",
+    "url": "/static/media/web.d0136fbc.png"
+  },
+  {
+    "revision": "b474c294eacce99eafe0dc8ba03568d6",
+    "url": "/static/media/wipaweResume.b474c294.pdf"
+  },
+  {
+    "revision": "24d8407dd6bb1464b9fe73451ba721d5",
+    "url": "/static/media/workout.24d8407d.gif"
+  },
+  {
+    "revision": "9a540608c438c13eeb8292a994b38b1d",
+    "url": "/static/media/workoutFP.9a540608.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.7de4b12a8d172f23637f5d7048cb4fae.js"
+  "/precache-manifest.f84ccfc3cdff59985069a5fb062c6aca.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
